79a7d3a292774589da31f331226f2eec9e90bef5
bazjerc <blaz.jerenec@gmail.com>
2022-11-24 19:25:39 +0100

    implement local validation for contact form

---
+++ js/contact-form.js
@@ -0,0 +1,142 @@
+// Page elements
+
+const contactForm = document.forms.contactForm;
+const inputName = contactForm.elements.name;
+const inputEmail = contactForm.elements.email;
+const inputMessage = contactForm.elements.message;
+
+// Validation message control
+
+const setValidationText = function (input, message) {
+  const validationText = contactForm.querySelector(
+    `#${input} + .contact-form__validation-text`
+  );
+  validationText.textContent = message;
+};
+
+const showValidationText = function (input) {
+  const validationText = contactForm.querySelector(
+    `#${input} + .contact-form__validation-text`
+  );
+  validationText.style.display = "block";
+};
+
+const hideValidationText = function (input) {
+  const validationText = contactForm.querySelector(
+    `#${input} + .contact-form__validation-text`
+  );
+  validationText.style.display = "none";
+};
+
+// Field validation logic
+
+const validateName = function (name) {
+  if (name.length === 0) {
+    inputName.classList.add("invalid");
+    setValidationText("name", "This field is required");
+    showValidationText("name");
+    return false;
+  }
+
+  const minLength = 3;
+
+  if (name.length < minLength) {
+    inputName.classList.add("invalid");
+    setValidationText(
+      "name",
+      `Entered name must be at least ${minLength} characters long`
+    );
+    showValidationText("name");
+    return false;
+  }
+
+  inputName.classList.remove("invalid");
+  setValidationText("name", "");
+  hideValidationText("name");
+  return true;
+};
+
+const validateEmail = function (email) {
+  if (email.length === 0) {
+    inputEmail.classList.add("invalid");
+    setValidationText("email", "This field is required");
+    showValidationText("email");
+    return false;
+  }
+
+  const emailRegex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+
+  if (!emailRegex.test(email)) {
+    setValidationText("email", "Entered email is invalid");
+    showValidationText("email");
+    return false;
+  }
+
+  inputEmail.classList.remove("invalid");
+  setValidationText("email", "");
+  hideValidationText("email");
+  return true;
+};
+
+const validateMessage = function (message) {
+  if (message.length === 0) {
+    inputMessage.classList.add("invalid");
+    setValidationText("message", "This field is required");
+    showValidationText("message");
+    return false;
+  }
+
+  const minLength = 10;
+
+  if (message.length < minLength) {
+    inputMessage.classList.add("invalid");
+    setValidationText(
+      "message",
+      `Entered message must be at least ${minLength} characters long`
+    );
+    showValidationText("message");
+    return false;
+  }
+
+  inputMessage.classList.remove("invalid");
+  setValidationText("message", "");
+  hideValidationText("message");
+  return true;
+};
+
+// Form control
+
+const resetForm = function () {
+  inputName.value = "";
+  inputEmail.value = "";
+  inputMessage.value = "";
+};
+
+const submitForm = function (e) {
+  e.preventDefault();
+
+  const name = inputName.value;
+  const email = inputEmail.value;
+  const message = inputMessage.value;
+
+  const isNameValid = validateName(name);
+  const isEmailValid = validateEmail(email);
+  const isMessageValid = validateMessage(message);
+
+  const isFormValid = isNameValid && isEmailValid && isMessageValid;
+
+  if (!isFormValid) return;
+
+  const formData = {
+    name,
+    email,
+    message,
+  };
+
+  resetForm();
+  this.querySelector(".contact-form__submit-btn").blur();
+};
+
+contactForm.addEventListener("submit", submitForm);
---
+++ pages/contact/contact.js
@@ -0,0 +1,1 @@
+import "../../js/contact-form.js";
