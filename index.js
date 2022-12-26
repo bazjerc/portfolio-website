dfb0df6dfb96ec492c32d4a3178e00688226a813
bazjerc <blaz.jerenec@gmail.com>
2022-12-26 12:37:30 +0100

    add scroll-to-bio functionality to index page

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 import "./js/nav.js";
-import "./js/social.js"
+import "./js/social.js";
+
+// Page elements
+
+const aboutButton = document.querySelector(".hero .icn-btn");
+
+// Scroll to bio logic
+
+const scrollToBio = function () {
+  const yOffset = -40;
+  const y =
+    window.pageYOffset +
+    document.querySelector(".card--bio").getBoundingClientRect().top +
+    yOffset;
+  window.scrollTo({ top: y, behavior: "smooth" });
+};
+
+// Event listeners
+
+aboutButton.addEventListener("click", scrollToBio);
