36afc940b72faa12da49678b4dc276b7dcaccf9a
bazjerc <blaz.jerenec@gmail.com>
2022-12-26 13:51:53 +0100

    add bio image grayscale filter effect, remove radial gradient effect

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import "./js/social.js";
 // Page elements
 
 const aboutButton = document.querySelector(".hero .icn-btn");
+const bioImage = document.querySelector(".card--bio img");
 
 // Scroll to bio logic
 
@@ -16,6 +17,23 @@ const scrollToBio = function () {
   window.scrollTo({ top: y, behavior: "smooth" });
 };
 
+// Bio image filter logic
+
+const bioImgObserver = new IntersectionObserver(
+  (entries, observer) => {
+    const entry = entries[0];
+    if (!sessionStorage.getItem("hasVisited")) {
+      entry.target.classList.add("grayscale");
+    }
+    if (!entry.isIntersecting) return;
+    sessionStorage.setItem("hasVisited", true);
+    entry.target.classList.remove("grayscale");
+    observer.unobserve(entry.target);
+  },
+  { root: null, rootMargin: "30px", threshold: 0 }
+);
+
 // Event listeners
 
 aboutButton.addEventListener("click", scrollToBio);
+bioImgObserver.observe(bioImage);
