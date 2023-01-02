42bb5bd4e986a400ad8290a60adef9a9a5c336e6
bazjerc <blaz.jerenec@gmail.com>
2023-01-02 15:12:11 +0100

    remove unused default assets

--- js/data/portfolio-projects.js
+++ js/data/portfolio-projects.js
@@ -1,25 +1,4 @@
 const projectsData = [
-  // {
-  //   id: "manage",
-  //   name: "Manage",
-  //   description:
-  //     "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.",
-  //   descriptionLong:
-  //     "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
-  //   keywords: {
-  //     category: ["Interaction Design", "Front End Development"],
-  //     tech: ["HTML", "CSS", "JS"],
-  //   },
-  //   figure: {
-  //     thumb: "image-portfolio-manage@2x.jpg",
-  //     hero: "image-manage-hero@2x.jpg",
-  //     preview: [
-  //       "image-manage-preview-1@2x.jpg",
-  //       "image-manage-preview-2@2x.jpg",
-  //     ],
-  //   },
-  //   url: "#",
-  // },
   {
     id: "xboard",
     name: "XBoard",
--- pages/portfolio/portfolio.js
+++ pages/portfolio/portfolio.js
@@ -73,37 +73,3 @@ const scaleProjectThumbnail = function (e) {
 // Event listeners
 
 projectsContainer.addEventListener("mouseover", scaleProjectThumbnail);
-
-// Markup
-// project card
-/* 
-<div class="card">
-  <div class="card__figure">
-    <img
-      src="../../assets/images/portfolio/image-portfolio-manage@2x.jpg"
-      alt="Hero"
-    />
-  </div>
-  <div class="card__content">
-    <div class="card__content-wrapper">
-      <div class="card__main">
-        <h2 class="card__title">About me</h2>
-        <p class="card__text">
-          I’m a junior front-end developer looking for a new role in
-          an exciting company. I focus on writing accessible HTML,
-          using modern CSS practices and writing clean JavaScript.
-          When writing JavaScript code, I mostly use React, but I can
-          adapt to whatever tools are required. I’m based in London,
-          UK, but I’m happy working remotely and have experience in
-          remote teams. When I’m not coding, you’ll find me outdoors.
-          I love being out in nature whether that’s going for a walk,
-          run or cycling. I’d love you to check out my work.
-        </p>
-      </div>
-      <div class="card__action">
-        <button class="btn">View project</button>
-      </div>
-    </div>
-  </div>
-</div>
-*/
